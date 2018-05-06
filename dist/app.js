65ad4ab9b63bd353ef20099d380d03b49dd18a4c
Sergii Grebeniuk <grebenyuksv@gmail.com>
2018-05-06 14:01:45 +0300

    Div created with ReactJS + JSX

--- dist/app.js
+++ dist/app.js
@@ -4,5 +4,9 @@ var reactRootDiv = document.createElement('div');
 document.body.appendChild(reactRootDiv);
 
 //	... and mount a React Component there
-var reactAppDiv = <div className="red">Div created with ReactJS + JSX</div>;
+var reactAppDiv = React.createElement(
+  "div",
+  { className: "red" },
+  "Div created with ReactJS + JSX"
+);
 ReactDOM.render(reactAppDiv, reactRootDiv);
