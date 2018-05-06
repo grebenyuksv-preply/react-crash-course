6d05f2562b9c8260fbae66a6c0039358fb37e12c
Sergii Grebeniuk <grebenyuksv@gmail.com>
2018-05-06 20:54:17 +0300

    The pure way

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment-timezone';
 
-export const Time = () => (
-	<div>{moment().format('HH:mm:ss')}</div>
+export const Time = ({ date, tz }) => (
+	<div>{moment.tz(date, tz).format('HH:mm:ss')}</div>
 );
 
+const tz = moment.tz.guess();
+
 function render() {
+	const now = moment();
 	ReactDOM.render(
 		<div className="red">
-			<Time />
+			<Time date={now} tz={tz} />
 		</div>,
 		document.getElementById('app'),
 	);
--- app.test.js
+++ app.test.js
@@ -5,12 +5,42 @@ import moment from 'moment-timezone';
 
 describe('<Time />', () => {
 	it('Formats the time properly', () => {
+		const date = moment.tz('2013-02-08 09:30:00', 'UTC');
+		const tz = 'Europe/Kiev';
 		const component = renderer
-			.create(<Time />)
+			.create(<Time date={date} tz={tz} />)
 			.toJSON();
 		console.log(component);
+		expect(component.children[0]).toBe('11:30:00');
+	});
 
-		//	no way to make it work
-		expect(component.children[0]).toBe("20:57:56"); 
+	it('Prints 24-hour format', () => {
+		const date = moment.tz('2013-06-08 16:30:00', 'UTC');
+		const tz = 'Europe/Kiev';
+		const component = renderer
+			.create(<Time date={date} tz={tz} />)
+			.toJSON();
+		console.log(component);
+		expect(component.children[0]).toBe('19:30:00');
+	});
+
+	it('Handles the Daylight Saving time', () => {
+		const date = moment.tz('2013-06-08 09:30:00', 'UTC');
+		const tz = 'Europe/Kiev';
+		const component = renderer
+			.create(<Time date={date} tz={tz} />)
+			.toJSON();
+		console.log(component);
+		expect(component.children[0]).toBe('12:30:00');
+	});
+
+	it('Works for different timezones', () => {
+		const date = moment.tz('2013-06-08 16:30:00', 'UTC');
+		const tz = 'CET'; // we like Berlin
+		const component = renderer
+			.create(<Time date={date} tz={tz} />)
+			.toJSON();
+		console.log(component);
+		expect(component.children[0]).toBe('18:30:00');
 	});
 });
