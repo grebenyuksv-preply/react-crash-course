836ec958a279f5e5107dfe4e262f3a755d34cbd6
Sergii Grebeniuk <grebenyuksv@gmail.com>
2018-05-06 21:54:42 +0300

    Add time format options

--- app.js
+++ app.js
@@ -2,17 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment-timezone';
 
-export const Time = ({ date, tz }) => (
-	<div>{moment.tz(date, tz).format('HH:mm:ss')}</div>
+export const Time = ({ date, tz, hoursFormat }) => {
+	const timeformat = hoursFormat === 24 ? 'HH:mm:ss' : 'hh:mm:ss';
+	return <div>{moment.tz(date, tz).format(timeformat)}</div>;
+};
+
+const SelectTimeFormat = ({ hoursFormat, onChange }) => (
+	<form>
+		{[12, 24].map(hoursFormatOption => (
+			<label>
+				<input
+					type="radio"
+					value={hoursFormatOption}
+					checked={hoursFormat === hoursFormatOption}
+					onChange={onChange}
+				/>
+				{hoursFormatOption}-hour
+			</label>
+		))}
+	</form>
 );
 
-const tz = moment.tz.guess();
+const state = {
+	tz: moment.tz.guess(),
+	hoursFormat: 24,
+	now: null,
+};
+
+function handleOptionChange(event) {
+	state.hoursFormat = parseInt(event.target.value, 10);
+}
+
+function tick() {
+	state.now = moment();
+}
 
 function render() {
-	const now = moment();
+	tick();
+	const { now, hoursFormat, tz } = state;
+
 	ReactDOM.render(
-		<div className="red">
-			<Time date={now} tz={tz} />
+		<div>
+			Time format:
+			<SelectTimeFormat
+				hoursFormat={hoursFormat}
+				onChange={handleOptionChange}
+			/>
+			Time:
+			<div className="red">
+				<Time date={now} tz={tz} hoursFormat={hoursFormat} />
+			</div>
 		</div>,
 		document.getElementById('app'),
 	);
--- app.test.js
+++ app.test.js
@@ -8,27 +8,37 @@ describe('<Time />', () => {
 		const date = moment.tz('2013-02-08 09:30:00', 'UTC');
 		const tz = 'Europe/Kiev';
 		const component = renderer
-			.create(<Time date={date} tz={tz} />)
+			.create(<Time date={date} tz={tz} hoursFormat={24} />)
 			.toJSON();
 		console.log(component);
 		expect(component.children[0]).toBe('11:30:00');
 	});
 
-	it('Prints 24-hour format', () => {
+	it('Handles 24-hour format', () => {
 		const date = moment.tz('2013-06-08 16:30:00', 'UTC');
 		const tz = 'Europe/Kiev';
 		const component = renderer
-			.create(<Time date={date} tz={tz} />)
+			.create(<Time date={date} tz={tz} hoursFormat={24} />)
 			.toJSON();
 		console.log(component);
 		expect(component.children[0]).toBe('19:30:00');
+	});
+
+	it('Handles 12-hour format', () => {
+		const date = moment.tz('2013-06-08 16:30:00', 'UTC');
+		const tz = 'Europe/Kiev';
+		const component = renderer
+			.create(<Time date={date} tz={tz} hoursFormat={12} />)
+			.toJSON();
+		console.log(component);
+		expect(component.children[0]).toBe('07:30:00');
 	});
 
 	it('Handles the Daylight Saving time', () => {
 		const date = moment.tz('2013-06-08 09:30:00', 'UTC');
 		const tz = 'Europe/Kiev';
 		const component = renderer
-			.create(<Time date={date} tz={tz} />)
+			.create(<Time date={date} tz={tz} hoursFormat={24} />)
 			.toJSON();
 		console.log(component);
 		expect(component.children[0]).toBe('12:30:00');
@@ -38,7 +48,7 @@ describe('<Time />', () => {
 		const date = moment.tz('2013-06-08 16:30:00', 'UTC');
 		const tz = 'CET'; // we like Berlin
 		const component = renderer
-			.create(<Time date={date} tz={tz} />)
+			.create(<Time date={date} tz={tz} hoursFormat={24} />)
 			.toJSON();
 		console.log(component);
 		expect(component.children[0]).toBe('18:30:00');
